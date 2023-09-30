1e45448cde9b5171b20a4e21c2f8b0e592266f47
Nicko <Nickozero3@gmail.com>
2023-09-30 13:38:13 -0300

    a

--- assets/productsArray.js
+++ assets/productsArray.js
@@ -5,7 +5,7 @@ export let productsData = [
     name: "100 + 10 Diamantes",
     category: "Diamantes",
     type: "Contenido Digital",
-    cost: 1100,
+    cost: 1310,
     cardImg: "../img/100.png",
     ageRange: "Diamantes",
   },
@@ -14,7 +14,7 @@ export let productsData = [
     name: "310 + 31 Diamantes",
     category: "Diamantes",
     type: "Contenido Digital",
-    cost: 3240,
+    cost: 3850,
     cardImg: "../img/310.png",
     ageRange: "Diamantes"
   },
@@ -23,7 +23,7 @@ export let productsData = [
     name: "520 + 52 Diamantes",
     category: "Diamantes",
     type: "Contenido Digital",
-    cost: 5280,
+    cost: 6260,
     cardImg: "../img/520.png",
     ageRange: "Diamantes"
   },
@@ -32,7 +32,7 @@ export let productsData = [
     name: "1060 + 106 Diamantes",
     category: "Diamantes",
     type: "Contenido Digital",
-    cost: 10300,
+    cost: 12200,
     cardImg: "../img/1060.png",
     ageRange: "Diamantes"
   },
@@ -41,7 +41,7 @@ export let productsData = [
     name: "2180 +218 Diamantes",
     category: "Diamantes",
     type: "Contenido Digital",
-    cost: 20090,
+    cost: 23800,
     cardImg: "../img/2180.png",
     ageRange: "Diamantes"
   },{
@@ -49,7 +49,7 @@ export let productsData = [
     name: "5600 + 560 Diamantes",
     category: "Diamantes",
     type: "Contenido Digital",
-    cost: 48900,
+    cost: 58000,
     cardImg: "../img/5600.png",
     ageRange: "Diamantes"
   },
@@ -58,7 +58,7 @@ export let productsData = [
     name: "ENSEÑANZA",
     category: "TEORICO",
     type: "Contenido Digital",
-    cost: 25000,
+    cost: 30000,
     cardImg: "../img/pdf.png",
     ageRange: "Especiales"
   }
